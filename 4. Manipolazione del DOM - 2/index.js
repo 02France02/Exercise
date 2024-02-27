ace27cbef1221f6a752c735d4f2b5804115b4c06
Francesco foti <149542938+02France02@users.noreply.github.com>
2024-02-27 20:44:16 +0100

    ES 2

---
+++ 4. Manipolazione del DOM - 2/index.js
@@ -0,0 +1,14 @@
+/* 
+-Cerca tutti gli elementi con classe "paragrafo".
+-Cerca l' elemento con id "elemento-3"
+-Cerca tutti gli elementi h2 della pagina  */ 
+
+const paragrafo = document.querySelectorAll(".paragrafo");
+console.log(paragrafo);
+
+const element = document.getElementById("elemento-3");
+console.log(element);
+
+const allH2 = document.querySelectorAll("h2")
+console.log(allH2);
+
