f563759d34e1a936db578ec1331848c93a57e12d
Francesco foti <149542938+02France02@users.noreply.github.com>
2024-03-22 11:35:39 +0100

    Handling Event 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 /* 
--Costruire un Clock componente che rende l'ora corrente entro un h2 etichetta. 
--Usa il useEffect aggancia per aggiornare l'ora ogni secondo.
- */
 
-import { Clock } from "./Clock";
+-Aggiungi un'immagine all'interno del pulsante e aggiungi un gestore di eventi onClick che stampi l'attributo src 
+ dell'immagine nella console.
+-Come puoi evitare che l'attributo name del pulsante venga stampato nella console quando si clicca sull'immagine? */
 
+import MouseClicker from "./MouseClicker";
+const image = "https://png.pngtree.com/png-clipart/20190516/original/pngtree-check-mark-icon-png-image_3566317.jpg"
 function App() {
   return (
     <>
-      <Clock/>
+        <MouseClicker name="One"><img style={{width: "100%"}} src={image} alt="icon"/></MouseClicker>
     </>
   );
 }
