80f785905572d7c77f004b100d19a1548964b7a0
Francesco foti <149542938+02France02@users.noreply.github.com>
2024-03-23 14:17:32 +0100

    2 UseEffect

---
+++ src/src 2 UseEffect/App.jsx
@@ -0,0 +1,16 @@
+/* 
+-Costruire un Clock componente che rende l'ora corrente entro un h2 etichetta. 
+-Usa il useEffect aggancia per aggiornare l'ora ogni secondo.
+ */
+
+import { Clock } from "./Clock";
+
+function App() {
+  return (
+    <>
+      <Clock/>
+    </>
+  );
+}
+
+export default App;
