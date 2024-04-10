5bc9714cbe35ff934f43b42e83bea06ca1617b36
Francesco foti <149542938+02France02@users.noreply.github.com>
2024-04-10 14:17:08 +0200

    src librerie 4

---
+++ src librerie 4/Components/Welcome.jsx
@@ -0,0 +1,21 @@
+import { useNavigate, Link } from "react-router-dom";
+
+function Welcome({ name = "guest"}) {
+  const navigateCount = useNavigate();
+
+  function handleMoveCounter() {
+    navigateCount("/counter");
+  }
+
+  return (
+    <>
+      <h1>{name}</h1>
+      <button onClick={handleMoveCounter}>Counter</button>
+      <Link to={"users/02France02"}>GITHUB</Link>
+
+
+    </>
+  );
+}
+
+export default Welcome;
